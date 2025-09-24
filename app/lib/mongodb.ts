83584afea0fa2397377f0b37550b4244143124fd
frank <wanyoike39@gmail.com>
2025-09-24 15:21:55 +0300

    add dependencies and add mongodb logic

---
+++ app/lib/mongodb.ts
@@ -0,0 +1,22 @@
+import { MongoClient } from 'mongodb';
+
+const url = process.env.MONGODB_URI!;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) throw new Error("please add mongo URI to .env");
+
+if (process.env.NODE_ENV === "development") {
+    if (!(global as any)._mongoClientPromise) {
+        client = new MongoClient(url, options);
+        (global as any)._mongoClientPromise = client.connect();
+    }
+    clientPromise = (global as any)._mongoClientPromise;
+} else {
+    client = new MongoClient(url, options);
+    clientPromise = client.connect();
+}
+
+export default clientPromise;
